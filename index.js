4f246c6a1bae2606109e9138ce7a68a43af85553
Shipon Islam <78357259+shipon-islam@users.noreply.github.com>
2023-09-20 21:55:49 +0600

    Update index.js added cors manualy

--- index.js
+++ index.js
@@ -18,16 +18,33 @@ const productRouter = require("./Routes/productRoute");
 const paymentRoutes = require("./Routes/paymentRoutes");
 //port setup
 const PORT = process.env.PORT || 5000;
-const corsOptions ={
-   origin:'*', 
-   credentials:true,            //access-control-allow-credentials:true
-   optionSuccessStatus:200,
-}
 
 //parse body setup
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
+
+//cors setup manualy because cors pakages not working
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://your-frontend.com"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Private-Network", true);
+  //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
+  res.setHeader("Access-Control-Max-Age", 7200);
+
+  next();
+});
 
 app.use("/api", userRouter);
 app.use("/api", productRouter);
